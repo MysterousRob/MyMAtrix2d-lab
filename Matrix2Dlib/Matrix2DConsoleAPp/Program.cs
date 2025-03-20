83d31d2f8e1d8894e57064fc222a0a85c4a32690
Robert Gąsior <robertjohngasior@gmail.com>
2025-03-20 17:34:18 +0100

    made changes work with the instructions and it works

--- Matrix2Dlib/Matrix2DConsoleAPp/Program.cs
+++ Matrix2Dlib/Matrix2DConsoleAPp/Program.cs
@@ -10,3 +10,45 @@ Console.WriteLine(m1);
 
 var m2 = Matrix2d.Transpose(m1);
 var m3 = (int[,])m2;
+
+
+var identityMatrix = Matrix2d.Id;
+Console.WriteLine($"Identity Matrix: {identityMatrix}");
+
+var zeroMatrix = Matrix2d.Zero;
+Console.WriteLine($"Zero Matrix: {zeroMatrix}");
+
+var m1 = new Matrix2d(1, 2, 3, 4);
+var m2 = new Matrix2d(5, 6, 7, 8);
+
+Console.WriteLine($"Matrix 1: {m1}");
+Console.WriteLine($"Matrix 2: {m2}");
+
+Console.WriteLine($"Matrix 1 + Matrix 2: {m1 + m2}");
+Console.WriteLine($"Matrix 1 - Matrix 2: {m1 - m2}");
+Console.WriteLine($"Matrix 1 * Matrix 2: {m1 * m2}");
+
+Console.WriteLine($"Transpose of Matrix 1: {Matrix2d.Transpose(m1)}");
+Console.WriteLine($"Determinant of Matrix 1: {m1.Det()}");
+
+// Scalar multiplication
+Console.WriteLine($"3 * Matrix 1: {3 * m1}");
+Console.WriteLine($"Matrix 1 * 3: {m1 * 3}");
+
+// Negation
+Console.WriteLine($"-Matrix 1: {-m1}");
+
+// Conversion to int[,]
+int[,] array = (int[,])m1;
+Console.WriteLine($"Matrix as Array: [[{array[0, 0]}, {array[0, 1]}], [{array[1, 0]}, {array[1, 1]}]]");
+
+// Parsing from string
+try
+{
+    var parsedMatrix = Matrix2d.Parse("[[2,1],[3,2]]");
+    Console.WriteLine($"Parsed Matrix: {parsedMatrix}");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"Error parsing matrix: {ex.Message}");
+}
--- Matrix2Dlib/Matrix2Dlib/Class1.cs
+++ Matrix2Dlib/Matrix2Dlib/Class1.cs
@@ -1,21 +1,27 @@
 ﻿#nullable disable
-using System.Transactions;
+using System;
 
 namespace Matrix2dLib
-
-
 {
-    public class Matrix2d : IEquatable<Matrix2d> 
+    
+    public class Matrix2d : IEquatable<Matrix2d>
     {
-        int a, b, c, d; // rivate fields
+        private readonly int a, b, c, d; 
+
         /*
+         Matrix representation:
          -------
          | a b |
          | c d |
          -------
-         */
+        */
 
-        public Matrix2d(int a, int b, int c, int d) 
+       
+        public static readonly Matrix2d Id = new Matrix2d(1, 0, 0, 1);
+        public static readonly Matrix2d Zero = new Matrix2d(0, 0, 0, 0);
+
+   
+        public Matrix2d(int a, int b, int c, int d)
         {
             this.a = a;
             this.b = b;
@@ -23,64 +29,74 @@ namespace Matrix2dLib
             this.d = d;
         }
 
-        public Matrix2d() : this(1, 0, 0, 1) { }
+        public Matrix2d() : this(1, 0, 0, 1) { } 
 
+       
+        public override string ToString() => $"[[{a}, {b}], [{c}, {d}]]";
 
-        public Matrix2d Id => new Matrix2d(1, 0, 0, 1); 
-
-
-        public Matrix2d Zero => new Matrix2d(0, 0, 0, 0);
-
-
-        public override string ToString() => $"[[{a},{b}],[{c},{d}]]";
-
+      
         public bool Equals(Matrix2d other)
         {
             if (other is null) return false;
-            return
-                a == other.a &&
-                b == other.b &&
-                c == other.c &&
-                d == other.d;
-
-            //throw new NotImplementedException();
+            return a == other.a && b == other.b && c == other.c && d == other.d;
         }
 
+        public override bool Equals(object obj) => obj is Matrix2d other && Equals(other);
 
-        public static bool operator ==(Matrix2d left, Matrix2d right)
-            => left.Equals(right);
+        public override int GetHashCode() => HashCode.Combine(a, b, c, d);
 
-        public static bool operator !=(Matrix2d left, Matrix2d right)
-            => !(left == right);
+        public static bool operator ==(Matrix2d left, Matrix2d right) => left.Equals(right);
+        public static bool operator !=(Matrix2d left, Matrix2d right) => !(left == right);
 
-   
+        
         public static Matrix2d operator +(Matrix2d left, Matrix2d right)
             => new Matrix2d(left.a + right.a, left.b + right.b,
-                            left.c + right.c, left.d + right.d);
+                             left.c + right.c, left.d + right.d);
 
-     
+        
+        public static Matrix2d operator -(Matrix2d left, Matrix2d right)
+            => new Matrix2d(left.a - right.a, left.b - right.b,
+                             left.c - right.c, left.d - right.d);
+
+      
         public static Matrix2d operator *(Matrix2d left, Matrix2d right)
             => new Matrix2d(left.a * right.a + left.b * right.c,
-                            left.a * right.b + left.b * right.d,
-                            left.c * right.a + left.d * right.c,
-                            left.c * right.b + left.d * right.d);
+                             left.a * right.b + left.b * right.d,
+                             left.c * right.a + left.d * right.c,
+                             left.c * right.b + left.d * right.d);
 
+        public static Matrix2d operator *(int k, Matrix2d m)
+            => new Matrix2d(k * m.a, k * m.b, k * m.c, k * m.d);
 
+        public static Matrix2d operator *(Matrix2d m, int k) => k * m;
 
-        public static Matrix2d operator *(int k, Matrix2d macierz)
-            => new Matrix2d(k * macierz.a, k * macierz.b,
-                            k * macierz.c, k * macierz.d);
-
-        public static Matrix2d operator *(Matrix2d macierz, int k)
-            => k * macierz;
-
+        public static Matrix2d operator -(Matrix2d m)
+            => new Matrix2d(-m.a, -m.b, -m.c, -m.d);
 
         public static Matrix2d Transpose(Matrix2d m)
             => new Matrix2d(m.a, m.c, m.b, m.d);
 
+        public int Det() => a * d - b * c;
+
+        public static int Determinant(Matrix2d m) => m.a * m.d - m.b * m.c;
+
         public static explicit operator int[,](Matrix2d m)
             => new int[2, 2] { { m.a, m.b }, { m.c, m.d } };
 
-
+        public static Matrix2d Parse(string input)
+        {
+            try
+            {
+                input = input.Replace(" ", "").Replace("[[", "").Replace("]]", "");
+                var parts = input.Split(new[] { "],[" }, StringSplitOptions.None);
+                var row1 = parts[0].Split(',').Select(int.Parse).ToArray();
+                var row2 = parts[1].Split(',').Select(int.Parse).ToArray();
+                return new Matrix2d(row1[0], row1[1], row2[0], row2[1]);
+            }
+            catch
+            {
+                throw new FormatException("Invalid matrix format.");
+            }
+        }
     }
 }

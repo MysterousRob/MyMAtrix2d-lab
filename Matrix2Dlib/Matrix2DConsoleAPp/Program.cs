fa9cff534698ad470f9262271300f50a6360e621
Robert Gąsior <robertjohngasior@gmail.com>
2025-03-20 18:40:42 +0100

    tests still dont work but goot rid of the errors

--- Matrix2Dlib/Matrix2DConsoleAPp/Program.cs
+++ Matrix2Dlib/Matrix2DConsoleAPp/Program.cs
@@ -1,16 +1,6 @@
 ﻿using Matrix2dLib;
 
 
-var m = new Matrix2d();
-Console.WriteLine(m);
-
-var m1 = new Matrix2d(1, 2, 3, 4);
-Console.WriteLine(m1);
-
-
-var m2 = Matrix2d.Transpose(m1);
-var m3 = (int[,])m2;
-
 
 var identityMatrix = Matrix2d.Id;
 Console.WriteLine($"Identity Matrix: {identityMatrix}");

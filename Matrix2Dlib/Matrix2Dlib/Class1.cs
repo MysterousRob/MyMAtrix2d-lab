ce509a7712f968e0e8b270e68d9266e052e3fa59
Robert Gąsior <robertjohngasior@gmail.com>
2025-03-20 17:16:24 +0100

    got rid of most comments

--- Matrix2Dlib/Matrix2Dlib/Class1.cs
+++ Matrix2Dlib/Matrix2Dlib/Class1.cs
@@ -3,9 +3,9 @@ using System.Transactions;
 
 namespace Matrix2dLib
 
-// immutable class
+
 {
-    public class Matrix2d : IEquatable<Matrix2d> // implementacja interfejsu IEquatable
+    public class Matrix2d : IEquatable<Matrix2d> 
     {
         int a, b, c, d; // rivate fields
         /*
@@ -15,7 +15,7 @@ namespace Matrix2dLib
          -------
          */
 
-        public Matrix2d(int a, int b, int c, int d) // constructor
+        public Matrix2d(int a, int b, int c, int d) 
         {
             this.a = a;
             this.b = b;
@@ -23,13 +23,13 @@ namespace Matrix2dLib
             this.d = d;
         }
 
-        public Matrix2d() : this(1, 0, 0, 1) { } // default constructor
+        public Matrix2d() : this(1, 0, 0, 1) { }
 
 
-        public Matrix2d Id => new Matrix2d(1, 0, 0, 1); // property typu get
+        public Matrix2d Id => new Matrix2d(1, 0, 0, 1); 
 
 
-        public Matrix2d Zero => new Matrix2d(0, 0, 0, 0); // property typu get
+        public Matrix2d Zero => new Matrix2d(0, 0, 0, 0);
 
 
         public override string ToString() => $"[[{a},{b}],[{c},{d}]]";
@@ -53,12 +53,12 @@ namespace Matrix2dLib
         public static bool operator !=(Matrix2d left, Matrix2d right)
             => !(left == right);
 
-        //dodawanie macierzy 
+   
         public static Matrix2d operator +(Matrix2d left, Matrix2d right)
             => new Matrix2d(left.a + right.a, left.b + right.b,
                             left.c + right.c, left.d + right.d);
 
-        // mnożenie macierzy
+     
         public static Matrix2d operator *(Matrix2d left, Matrix2d right)
             => new Matrix2d(left.a * right.a + left.b * right.c,
                             left.a * right.b + left.b * right.d,
@@ -66,7 +66,6 @@ namespace Matrix2dLib
                             left.c * right.b + left.d * right.d);
 
 
-        // mnożenie macierzy przez liczb
 
         public static Matrix2d operator *(int k, Matrix2d macierz)
             => new Matrix2d(k * macierz.a, k * macierz.b,
@@ -79,7 +78,6 @@ namespace Matrix2dLib
         public static Matrix2d Transpose(Matrix2d m)
             => new Matrix2d(m.a, m.c, m.b, m.d);
 
-        //rzutowanie na int [2,2]
         public static explicit operator int[,](Matrix2d m)
             => new int[2, 2] { { m.a, m.b }, { m.c, m.d } };
 
